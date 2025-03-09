3bad132b31a5ecadf7a0fd184195f00965dd5abf
unknown <lucas.ru74@gmail.com>
2025-03-09 10:56:01 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { MenuComponent } from './components/menu/menu.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
+
 export class AppComponent {
   title = 'My_PortFolio';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
 export const routes: Routes = [];
